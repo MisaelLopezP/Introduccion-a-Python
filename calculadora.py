41c64a279126a3914bc3241e8d9c948a55424429
MisaelLopezP <misael.lopez.p.mx@gmail.com>
2022-09-28 21:56:19 -0500

    Ejercicio modulo calculadora

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+import operaciones
+operaciones.sumar(5,5)
+operaciones.restar(5,5)
+operaciones.multiplicar(5,5)
+operaciones.dividir(5,5)
---
+++ operaciones.py
@@ -0,0 +1,8 @@
+def sumar (num1,num2):
+    print ("La suma de los dos numeros es: ", num1+num2)
+def restar (num1,num2):
+    print ("La suma de los dos numeros es: ", num1-num2)
+def multiplicar (num1,num2):
+    print ("La suma de los dos numeros es: ", num1*num2)
+def dividir (num1,num2):
+    print ("La suma de los dos numeros es: ", num1/num2)
